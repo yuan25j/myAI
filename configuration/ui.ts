6109c58fa12296ab3323c68a8479b5bc75984107
John Yuan <89277444+yuan25j@users.noreply.github.com>
2025-03-05 11:12:35 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Chat with John's AI Portfolio Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by John Yuan @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Reset Chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Chat with Jonny`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
