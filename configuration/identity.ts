7ac381fe77adbcc735955f792c973c4185cae134
John Yuan <89277444+yuan25j@users.noreply.github.com>
2025-03-05 11:13:18 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `John Yuan's`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `MS Student in CS at UNC`;
 
 export const AI_NAME: string = `Jonny`;
 export const AI_TONE: string = `[AI TONE]`;
