58c0e9d9475221afaec901f1f67a08e1c32d3356
John Yuan <89277444+yuan25j@users.noreply.github.com>
2025-03-05 13:08:10 -0500

    Update models.ts

--- configuration/models.ts
+++ configuration/models.ts
@@ -1,19 +1,19 @@
 import { ProviderName } from "@/types";
 
-export const INTENTION_MODEL: string = "o1-mini";
+export const INTENTION_MODEL: string = "gpt-4o-mini";
 export const INTENTION_TEMPERATURE: number = 0.7;
 
 export const RANDOM_RESPONSE_PROVIDER: ProviderName = "openai";
-export const RANDOM_RESPONSE_MODEL: string = "o1-mini";
+export const RANDOM_RESPONSE_MODEL: string = "gpt-4o-mini";
 export const RANDOM_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const HOSTILE_RESPONSE_PROVIDER: ProviderName = "openai";
-export const HOSTILE_RESPONSE_MODEL: string = "o1-mini";
+export const HOSTILE_RESPONSE_MODEL: string = "gpt-4o-mini";
 export const HOSTILE_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const QUESTION_RESPONSE_PROVIDER: ProviderName = "openai";
-export const QUESTION_RESPONSE_MODEL: string = "o1-mini";
+export const QUESTION_RESPONSE_MODEL: string = "gpt-4o";
 export const QUESTION_RESPONSE_TEMPERATURE: number = 0.7;
 
-export const HYDE_MODEL: string = "o1-mini";
+export const HYDE_MODEL: string = "gpt-4o-mini";
 export const HYDE_TEMPERATURE: number = 0.7;
