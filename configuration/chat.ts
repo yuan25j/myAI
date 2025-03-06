d1a801b3eb7d0a2a36c6bc2f1bc09d9c1def8866
John Yuan <89277444+yuan25j@users.noreply.github.com>
2025-03-06 12:50:48 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -2,6 +2,6 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Hi, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const WORD_CUTOFF: number = 15000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
