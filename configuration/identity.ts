34d11bfccc6c448915056310155ada7aa7ccf8d5
John Yuan <89277444+yuan25j@users.noreply.github.com>
2025-03-06 12:41:52 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `John Yuan's`;
-export const OWNER_DESCRIPTION: string = `MS Student in CS at UNC`;
+export const OWNER_NAME: string = `John Yuan`;
+export const OWNER_DESCRIPTION: string = `MS Student in Computer Science at UNC Chapel Hill`;
 
 export const AI_NAME: string = `Rameses`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Professional, informative, and friendly`;
+export const AI_ROLE: string = `Interactive resume assistant that provides detailed insights into John Yuan's professional background for recruiters`;
